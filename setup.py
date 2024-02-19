8f275159c1b90d4b64319bbbc263d0b8c012dc02
asdelaney <andre.delaney@gmail.com>
2024-02-18 22:54:28 -0500

    Updating setup file to include additional required info

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='yourpackagename',
+    name='nyt_assignment',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
+        'requests',
+        'matplotlib',
+        'pyyaml',
+        'pandas',
+        'wordcloud'
+    ],
+    entry_points={
+        'console_scripts': [
+            'nyt_assignment = nyt_assignment.nyt_assignment:main',
+        ],
+    },
+    author='Andre Delaney',
+    author_email='andre.delaney@gmail.com',
+    description='Package for analysis of NYT articles',
+    long_description='Package for analysis of NYT articles. Calculates mean of word count nad produces a plot',
+    url='https://github.com/ASDelaney/nyt_assignment',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
